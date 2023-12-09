72aec42b479f9af2bb9585bbba6beb1b6aa7bb18
vitnsk <121999199+vitnsk@users.noreply.github.com>
2023-12-09 10:16:59 +0700

    Start

---
+++ script.js
@@ -0,0 +1,28 @@
+
+new Vue({
+    el: '#app',
+    data: {
+        newTask: '',
+        todoList: []
+    },
+    methods: {
+            addTask() {
+                if (this.newTask.trim() !== ''){
+                    this.todoList.push(this.newTask)
+                    this.newTask = ''
+                }
+            },
+            removeTask(index) {
+                this.todoList.splice(index, 1)
+            }
+        },
+
+        mounted(){
+        
+            fetch('https://jsonplaceholder.typicode.com/todos/')
+            .then((response)=>response.json())
+            .then((json)=>(this.todoList=json));
+        
+        
+            }
+})
